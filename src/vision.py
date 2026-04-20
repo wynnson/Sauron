0b341865baf572c45f5a8072c0fb4373135a8310
wynnson <94803509+wynnson@users.noreply.github.com>
2026-04-20 14:56:39 -0400

    Vision Upgrade CV2 (#5)

--- src/vision.py
+++ src/vision.py
@@ -1,5 +1,17 @@
 from picamera2 import Picamera2
+import cv2
+
 picam2 = Picamera2()
 picam2.start()
-frame = picam2.capture_array()
-print(frame.shape)
+
+while True:
+    frame = picam2.capture_array()
+    frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
+
+    cv2.imshow("Pi Camera Stream", frame_bgr)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+picam2.close()
