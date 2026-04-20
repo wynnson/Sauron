0ede191fcc20f296b1c023b31b864a3a518be62f
wynnson <wynnsondaone@gmail.com>
2026-04-20 14:09:41 -0400

    GPS

--- src/main.py
+++ src/main.py
@@ -3,3 +3,16 @@ from pymavlink import mavutil
 master = mavutil.mavlink_connection('/dev/serial0', baud=921600)
 master.wait_heartbeat()
 print("Connected")
+
+msg = master.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+print(msg)
+
+lat = msg.lat / 1e7
+lon = msg.lon / 1e7
+alt_m = msg.alt / 1000.0          # altitude above mean sea level
+rel_alt_m = msg.relative_alt / 1000.0
+
+print("lat:", lat)
+print("lon:", lon)
+print("alt_m:", alt_m)
+print("relative_alt_m:", rel_alt_m)
